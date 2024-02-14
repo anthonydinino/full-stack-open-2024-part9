2d1a112234254277d2a65e83443ae9d2c5d5d99b
anthonydinino <adinino1996@gmail.com>
2024-02-14 19:41:55 +1030

    Exercise 9.3

--- bmiCalculator.ts
+++ bmiCalculator.ts
@@ -1,5 +1,27 @@
+interface BmiValues {
+  height: number;
+  weight: number;
+}
+
+const parseArguments = (args: string[]): BmiValues => {
+  if (args.length > 4) {
+    throw new Error("Too many arguments");
+  }
+  if (args.length < 4) {
+    throw new Error("Not enough arguments");
+  }
+
+  if (!isNaN(Number(args[2])) && !isNaN(Number(args[3]))) {
+    return {
+      height: Number(args[2]),
+      weight: Number(args[3]),
+    };
+  } else {
+    throw new Error("Provided values were not numbers!");
+  }
+};
+
 /**
- * Calculates bmi based on height and weight
  * @param height in centimeters
  * @param weight in kilograms
  * @returns string
@@ -7,18 +29,19 @@
 const calculateBmi = (height: number, weight: number): string => {
   const bmi = weight / (height / 100) ** 2;
   if (bmi < 18.5) {
-    return "Underweight (not healthy weight)";
+    return "Underweight";
   }
   if (bmi < 24.9) {
     return "Normal (healthy weight)";
   }
   if (bmi < 29.9) {
-    return "Overwight (not healthy weight)";
+    return "Overwight";
   }
   if (bmi > 30) {
-    return "Obese (not healthy weight)";
+    return "Obese";
   }
   return "Something went wrong with calculation";
 };
 
-console.log(calculateBmi(180, 74));
+const { height, weight }: BmiValues = parseArguments(process.argv);
+console.log(calculateBmi(height, weight));
